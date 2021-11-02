082e2082a6340782aafebe0bb94eb380bb89c69f
Myles Berueda <myles.berueda@codemithril.com>
2021-11-01 16:12:52 -1000

    boilerplate for day-09

---
+++ 30-days-of-code/day-09-recursion-3.cpp
@@ -0,0 +1,69 @@
+/*
+ * File: /day-09-recursion-3.cpp
+ * Project: 30-days-of-code
+ * Created Date: Monday November 1st 2021
+ * Author: Myles Berueda
+ * -----
+ * Last Modified: Monday November 1st 2021 12:03:50 pm
+ * -----
+ * Copyright (c) 2021 MylesWritesCode
+ * -----
+ * HISTORY
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'factorial' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+int factorial(int n) {
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    int result = factorial(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
