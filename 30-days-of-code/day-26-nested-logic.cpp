5ec92836b8865085b30b641e586c089f9910528c
Myles Berueda <myles.berueda@codemithril.com>
2021-11-08 00:43:21 -0800

    added day-26

---
+++ 30-days-of-code/day-26-nested-logic.cpp
@@ -0,0 +1,27 @@
+/**
+ * File: /day-26-nested-logic.cpp
+ * Project: 30-days-of-code
+ * Purpose: // TODO: Add the purpose of the file here.
+ *
+ * @author Myles Berueda
+ * @date   Sunday November 7th 2021
+ * -----
+ * Modified: Sunday November 7th 2021 11:34:21 pm
+ * -----
+ * Copyright (c) 2021 MylesWritesCode
+ * -----
+ * HISTORY
+ **/
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+  
+  return 0;
+}
